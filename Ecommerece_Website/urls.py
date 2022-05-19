3f8e942af08230d334346a8368fc7644e57a5cbb
Zain Kiyani <zainkiyani682@gmail.com>
2022-05-19 15:37:26 +0500

    add gitignore file

--- Ecommerece_Website/urls.py
+++ Ecommerece_Website/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
 # from django.views.static import serve
 # from django.conf.urls import url
 
@@ -28,5 +29,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
